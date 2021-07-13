69f33d2a5154966cd835f4b660f4219c62c7ff4b
225141125 <80028798+225141125@users.noreply.github.com>
2021-07-13 16:47:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+const hamburgerMenu = document.querySelector('#navigation .nav-icon');
+const navContent = document.querySelector('#nav-content');
+const closeNavButton = document.querySelector('#nav-content .close-btn');
+const navLinks = document.querySelectorAll('#nav-content nav ul li a');
+const scrollButton = document.querySelector(".scroll-top");
+
+// scroll TOP Button Events
+
+if(scrollButton){
+  window.addEventListener('scroll', ()=> {
+    if(pageYOffset > (window.innerHeight * 1.2)){
+      scrollButton.style.display="flex";
+    }else{
+      scrollButton.style.display="none";
+    }
+  });
+  scrollButton.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
+}
+
+// Hamburger Menu events
+hamburgerMenu.addEventListener('click', ()=>{
+  navContent.classList.add('show');
+  document.body.style.overflow="hidden";
+});
+closeNavButton.addEventListener('click', ()=>{
+  navContent.classList.remove('show');
+  document.body.style.overflow="initial";
+});
+navLinks.forEach( link => {
+  link.addEventListener('click', ()=> {
+    navContent.classList.remove('show');
+    document.body.style.overflow="initial";
+  })
+})
+
+//emai submition
+
+
+    var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
